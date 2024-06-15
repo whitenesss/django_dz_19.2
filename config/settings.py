19cb0364e19461f8833b240ddc1299e00c503a82
whitenesss <144126783+whitenesss@users.noreply.github.com>
2024-06-16 00:12:12 +0300

    dz 22.2 test 3

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'catalog:index'
 LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = '/users/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
